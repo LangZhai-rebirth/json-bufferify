5f848bab40ab27f7ce8d8680680705c0ec192be5
LangZhai-rebirth <zl2012xyz@hotmail.com>
2025-03-11 14:39:18 +0800

    Update to 0.2.3.

--- dist/json-bufferify.min.js
+++ dist/json-bufferify.min.js
@@ -1,7 +1,7 @@
 /**
- * json-bufferify 0.2.2
- * Date: 2020-10-14
- * © 2017-2020 LangZhai(智能小菜菜)
+ * json-bufferify 0.2.3
+ * Date: 2025-03-11
+ * © 2017-2025 LangZhai(智能小菜菜)
  * This is licensed under the GNU LGPL, version 3 or later.
  * For details, see: https://www.gnu.org/licenses/lgpl.html
  * Project home: https://github.com/LangZhai-rebirth/json-bufferify
